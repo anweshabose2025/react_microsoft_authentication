25eb5399de7fd294f4fc8da56c882389b8d13553
Anwesha Bose <anwesha.bose@netwoven.com>
2026-03-12 02:37:43 +0530

    first commit

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: "3f515280-5f90-4f62-ab6a-eeb511212967",
     authority: "https://login.microsoftonline.com/ecea507e-d964-42f3-8825-5c1b5efca41f",
-    redirectUri: "https://lemon-mushroom-0914c620f.6.azurestaticapps.net"//window.location.origin//"http://localhost:5173"
+    redirectUri: window.location.origin//"http://localhost:5173"
   },
   cache: {
     cacheLocation: "localStorage",
